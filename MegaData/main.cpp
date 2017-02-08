e8e39457999bc533ef5f81f7182795818575a6a8
jackopuff <jackson@carterrealm.com>
2017-02-08 11:58:31 -0700

    Initial Commit

---
+++ MegaData/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MegaData
+//
+//  Created by Carter, Jackson on 2/8/17.
+//  Copyright © 2017 Carter, Jackson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
