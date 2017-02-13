60591950e7fa5e1254c2c19cfb6703c36f303b37
jackopuff <jackson@carterrealm.com>
2017-02-13 12:29:36 -0700

    added Node header

---
+++ MegaData/Model/Node.hpp
@@ -0,0 +1,22 @@
+
+
+#ifndef Node_hpp
+#define Node_hpp
+
+template <class type>
+class Node
+{
+private:
+    Type nodeData;
+    Node<Type> * nodePointer;
+public:
+    Node();
+    Node(Type nodeData);
+    Node(Type data, Node<Type> * next);
+    
+    void setNodeData(Type data);
+    void setNodePointer(Node<Type> pointer);
+    Type getNodeData();
+    Node<Type> * getNodePointer();
+};
+#endif /* Node_h*/
